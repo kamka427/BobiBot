53e57802edce23a27b1daed06372b86c022da054
Kálmán Neszlényi <kamka42711@gmail.com>
2021-01-27 11:49:26 +0100

    Upload for Repl.it testing

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Hello World!")
+import discord
+import os
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('{0.user} has connected to Discord!'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Yo!')
+
+
+
+
+client.run(os.getenv('TOKEN'))
