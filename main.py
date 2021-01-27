19dc7fe74b76d87cf7c54d313712c857e208919f
Kálmán Neszlényi <kamka42711@gmail.com>
2021-01-27 12:48:23 +0100

    Working dotenv import

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from dotenv import load_dotenv
 import discord
 import os
 
 client = discord.Client()
+
+
+load_dotenv()
 
 
 @client.event
@@ -14,10 +18,11 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
+    if message.content.startswith('hello'):
         await message.channel.send('Yo!')
 
+    if message.content.startswith('yo'):
+        await message.channel.send('yo yo yo!')
 
-
-
+# token = os.getenv('TOKEN')
 client.run(os.getenv('TOKEN'))
