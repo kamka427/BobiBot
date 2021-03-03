741f47fd247b5d1991134bd4eba1b6922cc12c26
Kálmán Neszlényi <kamka42711@gmail.com>
2021-03-03 11:42:41 +0100

    same

--- cogs/tiktok.py
+++ cogs/tiktok.py
@@ -1,3 +1,4 @@
+import os
 from discord.ext import commands
 import discord
 from pathlib import Path
@@ -8,19 +9,13 @@ from random import randint
 from selenium import webdriver
 
 # Optional argument, if not specified will search path.
-driver = webdriver.Chrome('chromedriver')
-driver.get('http://www.google.com/')
-# from selenium import webdriver
-# import os
-
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-# chrome_options.add_argument("--headless")
-# chrome_options.add_argument("--disable-dev-shm-usage")
-# chrome_options.add_argument("--no-sandbox")
-# driver = webdriver.Chrome(executable_path=os.environ.get(
-#     "CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-
+# driver = webdriver.Chrome('cogs/chromedriver')
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome('cogs/chromedriver', chrome_options = chrome_options)
+# driver.get('http://www.google.com/')
 
 class TikTok(commands.Cog):
 
