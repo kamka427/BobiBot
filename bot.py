5e6b303c5eba3bdadcf50a7d202bf73bbeeaca53
Kálmán Neszlényi <kamka42711@gmail.com>
2021-03-03 11:58:45 +0100

    .

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import asyncpraw
 # load_dotenv()  # !
 
 cogs = [
-    'music', 'reddit', 'messages', 'tiktok'
+    'music', 'reddit', 'messages'
 ]
 
 bot = commands.Bot(command_prefix="&", description="Bobi az ezermester")
