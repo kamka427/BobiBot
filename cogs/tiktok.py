955fa69183f38b5d66b240c4b6e3534cff495f3a
Kálmán Neszlényi <kamka42711@gmail.com>
2021-03-03 01:08:29 +0100

    heroku fix

--- cogs/tiktok.py
+++ cogs/tiktok.py
@@ -9,13 +9,13 @@ from selenium.webdriver import Chrome
 from selenium import webdriver
 import os
 
-# chrome_exec_shim = os.environ.get("GOOGLE_CHROME_BIN", "chromedriver")
-# opts = webdriver.ChromeOptions()
-# opts.binary_location = chrome_exec_shim
-# opts.add_argument('--disable-gpu')
-# opts.add_argument("--no-sandbox")
-# driver = webdriver.Chrome(
-#     executable_path='/app/development/chromedriver', chrome_options=opts)
+op = webdriver.ChromeOptions()
+op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+op.add_argument('--headless')
+op.add_argument("--no-sandbox")
+op.add_argument("--disable-dev-sh-usage")
+driver = webdriver.Chrome(
+    executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op)
 
 
 class TikTok(commands.Cog):
